e16e57a61beb166963d674e6843162c38efc9561
apuxiner <apuxiner@163.com>
2025-10-28 11:13:46 +0800

    add@初版

---
+++ pydpack/__main__.py
@@ -0,0 +1,78 @@
+"""
+@author axiner
+@version v0.0.1
+@created 2025/10/20 14:50
+@abstract
+@description
+@history
+"""
+import argparse
+import sys
+
+from toollib.pyd import PydPacker
+
+from pydpack import __version__
+
+prog = "pydpack"
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog=prog,
+        description="This is a pydpack.（pyd打包，可将py目录或文件打包成pyd(so)）",
+        epilog="examples: \n"
+               "  %(prog)s -s <py目录或文件> -e main.py\n"
+               "",
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"%(prog)s {__version__}")
+    parser.add_argument(
+        '-s',
+        '--src',
+        required=True,
+        type=str,
+        metavar="",
+        help='源（py目录或文件）')
+    parser.add_argument(
+        '-e',
+        '--exclude',
+        type=str,
+        metavar="",
+        help='排除编译（正则表达式，使用管道等注意加引号）')
+    parser.add_argument(
+        '-i',
+        '--ignore',
+        default='.git|.idea|.vscode|__pycache__',
+        type=str,
+        metavar="",
+        help='忽略复制（正则表达式，使用管道等注意加引号）')
+    parser.add_argument(
+        '--suffix',
+        default='Pyd',
+        type=str,
+        metavar="",
+        help='后缀（默认Pyd）')
+    parser.add_argument(
+        '--clean',
+        action='store_true',
+        help='是否清理（默认不清理）')
+    args = parser.parse_args()
+    src = args.src.strip()
+    if not src or src == "''":
+        sys.stderr.write('ERROR: -s/--src: 不能为空\n')
+        sys.exit(1)
+    packer = PydPacker(
+        src=src,
+        exclude=args.exclude,
+        ignore=args.ignore,
+        suffix=args.suffix,
+        is_clean=args.clean,
+    )
+    packer.run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,84 @@
+"""
+@author axiner
+@version v0.0.1
+@created 2025/10/20 14:50
+@abstract main
+@description
+@history
+"""
+import os
+import re
+from pathlib import Path
+from shutil import rmtree
+
+from setuptools import setup, Command
+
+here = Path(__file__).absolute().parent
+pkg_name = "pydpack"
+
+
+class BuildCommand(Command):
+    description = "Build the package(.whl)"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self._clean_dist_dirs()
+        self._update_version()
+        os.system("python -m build --wheel --no-isolation")
+
+    @staticmethod
+    def _clean_dist_dirs():
+        for path in ["dist", "build", f"{pkg_name}.egg-info"]:
+            try:
+                rmtree(here / path)
+            except FileNotFoundError:
+                pass
+
+    @staticmethod
+    def _update_version():
+        with open(".history", "r", encoding="utf8") as f:
+            __version__ = None
+            for line in f:
+                if line.startswith("## "):
+                    __version__ = line.replace("## ", "").strip(" \nv")
+                    break
+            if not __version__:
+                raise ValueError("Please set version")
+        init_file = here.joinpath(f"{pkg_name}/__init__.py")
+        with open(init_file, "r", encoding="utf8") as f:
+            content = f.read()
+        with open(init_file, "w", encoding="utf8", newline="\n") as f:
+            content = re.sub(r'__version__ = "[\d.]+"', rf'__version__ = "{__version__}"', content)
+            f.write(content)
+
+
+class PublishCommand(Command):
+    description = "Publish the package(.whl)"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system("python setup.py bld")
+        os.system("python -m twine upload dist/*")
+
+
+setup(
+    cmdclass={
+        "bld": BuildCommand,
+        "pub": PublishCommand,
+    },
+    options={
+        "bdist": {"plat_name": "any"},
+    },
+)
